c6fb160ffe7c33c1b494d4d3b31fab0162bcbed1
Hizz-el <hizzel865@gmail.com>
2025-09-25 19:42:29 +0700

    first commit

---
+++ contoh.py
@@ -0,0 +1,7 @@
+# Program sederhana untuk menyapa pengguna
+
+# Meminta input dari pengguna
+nama = input("Masukkan nama kamu: ")
+
+# Menyapa pengguna
+print(f"Halo, {nama}! Selamat belajar Python 😄")
